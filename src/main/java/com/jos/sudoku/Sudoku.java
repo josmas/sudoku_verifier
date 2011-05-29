91c43911da916468dee457aaf43f7397d5977e59
Josmas Flores <josmasflores@gmail.com>
2011-05-29 23:22:51 +0100

    Added creational test and exceptions on bad input

--- src/main/java/com/jos/sudoku/Sudoku.java
+++ src/main/java/com/jos/sudoku/Sudoku.java
@@ -14,21 +14,42 @@ public class Sudoku {
 		//TODO change these awful names!
 		String sudokuInARow = sudokuAsText.replaceAll("\n", "");
 		String sudokuInARow2 = sudokuInARow.replaceAll(" ", "");
-		System.out.println("In a row: " + sudokuInARow2);
+//		System.out.println("In a row: " + sudokuInARow2);
+		if (Sudoku.isValidChain(sudokuInARow2)){
+			throw new NumberFormatException("Illegal characters in the sudoku string");
+		}
+		if (!isTheRightAmountOFNumbers(sudokuInARow2)){
+			throw new NumberFormatException("Not Enough characters in the sudoku string");
+		}
+		
+		
 		int counterForSudokuText = 0;
 		for (int row = 0; row < 9; row++) {
 		    for (int column = 0; column < 9; column++) {
 		    	grid[row] [column] = new Integer(sudokuInARow2.charAt(counterForSudokuText)+"").intValue();
 		    	counterForSudokuText++;
-		        System.out.print(grid[row] [column] + " ");
+//		        System.out.print(grid[row] [column] + " ");
 		    }
-		    System.out.println();
+//		    System.out.println();
 		}
 		return grid;
 	}
 
 	int[][] getSudokuGrid() {
 		return sudokuGrid;
+	}
+	
+	/**
+	 * <p>Validity through RegExp: all numbers from 1 to 9 appear in the chain, just once</p>
+	 * @param row  the Sudoku chain to verify
+	 * @return A boolean indicating if the chain is valid
+	 */
+	static boolean isValidChain(String chain) {
+		return chain.matches("[1-9]{9}");
+	}
+	
+	private boolean isTheRightAmountOFNumbers(String chain){
+		return (chain.length() == 81);
 	}
 
 }
--- src/test/java/com/jos/sudoku/SudokuTest.java
+++ src/test/java/com/jos/sudoku/SudokuTest.java
@@ -19,5 +19,35 @@ public class SudokuTest {
 		assertEquals(1, sudokuGrid[8][8]);
 		
 	}
+	
+	@Test
+	public void sudokuInvalidCreationTest(){
+		String sudokuAsText = "248 39o 716\n571 628 349\n936 741 582\n" +
+		"682 539 174\n359 174 628\n714 862 953\n" +
+		"863 417 295\n195 286 437\n427 953 861\n";
+		
+		try {
+			new Sudoku(sudokuAsText);
+			fail("Should have thrown and exception");
+		}
+		catch(NumberFormatException nfe){
+			//ignore
+		}
+	}
+	
+	@Test
+	public void sudokuInvalidCreationNotEnoughInputInChain(){
+		//TODO repetition with previous test
+		String sudokuWithNotEnoughNumbers = "248 395 716\n571 628 349\n936 741 582\n" +
+		"863 417 295\n195 286 437\n427 953 861\n";
+		
+		try {
+			new Sudoku(sudokuWithNotEnoughNumbers);
+			fail("Should have thrown and exception");
+		}
+		catch(NumberFormatException nfe){
+			//ignore
+		}
+	}
 
 }
